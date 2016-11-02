8f90cc4868fa66c0bb832cb4f49111bef966bd7b
Atnanasi <ys2000pro@gmail.com>
2016-11-02 14:07:43 +0900

    Finished

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CeDiary::Application.config.secret_token = 'fe6d7576d9a440741f5200bd22a365c687ae0ba947c7486c07c77fd71f33d3dc865dc96103098b0337ba5d5d44e8f5a09e4f7b374516975f3de7cc3ab850f41e'
